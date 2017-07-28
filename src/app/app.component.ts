03b5c2ffb3d0b22247ec630148e7ea86b8c48bdd
urushiyama <aswif10flis1ntkb@gmail.com>
2017-07-28 17:08:02 +0900

    Finish Chapter 2: The Hero Editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   template: `
-    <h1>
-      Welcome to {{title}}!!
-    </h1>
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero:Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
